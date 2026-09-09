1584b4cb906cdef7d699551e3949b67db09f8510
Hiroshi Fujiwara <hiroshi.829f@gmail.com>
2026-09-09 06:25:12 +0000

    fix chapter07 for main

--- today-checker/index.js
+++
@@ -1,7 +0,0 @@
-import chalk from 'chalk';
-import dayjs from 'dayjs';
-
-const today = dayjs().format('YYYY-MM-DD');
-
-console.log(chalk.blue(`今日の日付: ${today}`));
-console.log(chalk.green('chalkとdayjs、2つのパッケージのインストールに成功しました！'));
